5ae99ea33cf0b0f03014616670253a61732f4c8f
Agustin Cicchitti <agucichii@gmail.com>
2022-02-27 22:56:47 +0100

    Log4Java Commit #21

--- src/main/java/com/sinensia/zara/mainZara.java
+++ src/main/java/com/sinensia/zara/mainZara.java
@@ -3,16 +3,17 @@ package com.sinensia.zara;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
-
+import org.apache.log4j.Logger;
 import com.sinensia.entidades.Datos;
 import com.sinensia.servicios.Metodos;
 
 public class mainZara {
 
+	private static Logger LogJava = Logger.getLogger(mainZara.class);
+
 	public static void main(String[] args) {
 
 		try {
-
 			Metodos met = new Metodos();
 			ArrayList<Datos> valores = met.leerValores(
 					"C:\\Users\\aguci\\eclipse-workspace\\zara-project\\src\\main\\resources\\stocks-ITX.csv");
@@ -21,9 +22,9 @@ public class mainZara {
 			ArrayList<Datos> compraAcc = met.diaCompra(valores, diaVier);
 			met.cantAcc(compraAcc);
 			met.ventAcc(valores);
-			
-		} catch (IOException e) {
 
+		} catch (IOException e) {
+			LogJava.error("An exception! Error!", e);
 		}
 
 	}
